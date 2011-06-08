ae7e98e45ab63e6dda53d43237dbd329c825852a
Fabio Mascarenhas <mascarenhas@ufrj.br>
2011-06-08 14:30:34 -0300

    fix tabs, const prop to not assume that phi args are variables

--- ssa/const.py
+++ ssa/const.py
@@ -3,6 +3,12 @@ from ssa import *
 TOP = "TOP"
 BOTTOM = "BOTTOM"
 
+def check_var(var, hash):
+    if is_var(var):
+        return hash[var]
+    else:
+        return int(var)
+
 def meet(v1, v2):
     if v1 == v2:
         return v1
@@ -26,39 +32,40 @@ def def_uses(blocks):
     for b in blocks:
         for (var, pairs) in b.phis.items():
            if var not in vars:
-	       vars.append(var)
-	   defs[var] = ["phi", var, pairs]
-	   for pair in pairs:
-               if pair[0] not in vars:
-                   vars.append(pair[0])
-	       if pair[0] not in uses:
-	           uses[pair[0]] = []
-	       uses[pair[0]].append((defs[var], b))
-	for op in b.ops:
+               vars.append(var)
+           defs[var] = ["phi", var, pairs]
+           for pair in pairs:
+               if is_var(pair[0]):
+                   if pair[0] not in vars:
+                       vars.append(pair[0])
+                   if pair[0] not in uses:
+                       uses[pair[0]] = []
+                   uses[pair[0]].append((defs[var], b))
+        for op in b.ops:
             tag = op[0]
-	    if tag == "br":
-	        break
-	    elif tag == "brc" or tag == "return":
-	        if is_var(op[1]):
-	            if op[1] not in uses:
-		        uses[op[1]] = []
-		    uses[op[1]].append((op,b))
-	    else:
-	        dest = op[1]
-	        if dest not in vars:
-		    vars.append(dest)
-		defs[dest] = op
-		if op[0] != "=":
-		    if is_var(op[3]):
-		        if op[3] not in uses:
-		            uses[op[3]] = []
-		        uses[op[3]].append((op,b))
-		if is_var(op[2]):
-		    if op[2] not in uses:
-		       uses[op[2]] = []
-		    uses[op[2]].append((op,b))
+            if tag == "br":
+                break
+            elif tag == "brc" or tag == "return":
+                if is_var(op[1]):
+                    if op[1] not in uses:
+                        uses[op[1]] = []
+                    uses[op[1]].append((op,b))
+            else:
+                dest = op[1]
+                if dest not in vars:
+                    vars.append(dest)
+                defs[dest] = op
+                if op[0] != "=":
+                    if is_var(op[3]):
+                        if op[3] not in uses:
+                            uses[op[3]] = []
+                        uses[op[3]].append((op,b))
+                if is_var(op[2]):
+                    if op[2] not in uses:
+                       uses[op[2]] = []
+                    uses[op[2]].append((op,b))
     return (vars, defs, uses)
-	 
+         
 def sscp(var, defs, uses):
     values = {}
     wl = []
@@ -66,28 +73,28 @@ def sscp(var, defs, uses):
         op = defs.get(v, None)
         if op and op[0] == "=" and not is_var(op[2]):
             values[v] = int(op[2])
-	    wl.append(v)
-	else:
-	    values[v] = TOP
+            wl.append(v)
+        else:
+            values[v] = TOP
     while len(wl) > 0:
         v = wl.pop(0)
         for (use, block) in uses.get(v, []):
-	    if use[0] == "=":
-	        val = values[use[2]]
-	    elif use[0] == "phi":
-	        val = TOP
-	        pairs = use[2]
-	        for pair in pairs:
-		    val = meet(val, values[pair[0]])
+            if use[0] == "=":
+                val = values[use[2]]
+            elif use[0] == "phi":
+                val = TOP
+                pairs = use[2]
+                for pair in pairs:
+                    val = meet(val, check_var(pair[0], values))
             elif use[0] == "brc" or use[0] == "return":
-	        continue
-	    else:
-	        left = is_var(use[2]) and values[use[2]] or int(use[2])
-	        right = is_var(use[3]) and values[use[3]] or int(use[3])
-	        val = arith(use[0], left, right)
-	    if val != values[use[1]]:
-	        values[use[1]] = val
-	        wl.append(use[1])
+                continue
+            else:
+                left = check_var(use[2], values)
+                right = check_var(use[3], values)
+                val = arith(use[0], left, right)
+            if val != values[use[1]]:
+                values[use[1]] = val
+                wl.append(use[1])
     return values
 
 
@@ -108,7 +115,7 @@ def sccp(blocks, vars, uses):
             for (var, pairs) in b.phis.items():
                 val = TOP
                 for pair in pairs:
-                    val = meet(val, values[pair[0]])
+                    val = meet(val, check_var(pair[0], values))
                 values[var] = val
                 for (use, block) in uses.get(var, []):
                     if block.live:
@@ -130,13 +137,13 @@ def sccp(blocks, vars, uses):
                 elif tag == "return":
                     continue
                 elif tag == "=":
-                    values[op[1]] = is_var(op[2]) and values[op[2]] or int(op[2])
+                    values[op[1]] = check_var(op[2], values)
                     for (use, block) in uses.get(op[1], []):
                         if block.live:
                             usewl.append(use)
                 else:
-                    left = is_var(op[2]) and values[op[2]] or int(op[2])
-                    right = is_var(op[3]) and values[op[3]] or int(op[3])
+                    left = check_var(op[2], values)
+                    right = check_var(op[3], values)
                     values[op[1]] = arith(tag, left, right)
                     for (use, block) in uses.get(op[1], []):
                         if block.live:
@@ -144,12 +151,12 @@ def sccp(blocks, vars, uses):
         while len(usewl) > 0:
             use = usewl.pop(0)
             if use[0] == "=":
-	        val = is_var(use[2]) and values[use[2]] or int(use[2])
-	    elif use[0] == "phi":
-	        val = TOP
-	        pairs = use[2]
-	        for pair in pairs:
-		    val = meet(val, values[pair[0]])
+                val = check_var(use[2], values)
+            elif use[0] == "phi":
+                val = TOP
+                pairs = use[2]
+                for pair in pairs:
+                    val = meet(val, check_var(pair[0], values))
             elif use[0] == "brc":
                 cond = values[use[1]]
                 if cond == BOTTOM:
@@ -157,14 +164,14 @@ def sccp(blocks, vars, uses):
                     cfgwl.append(use[3])
                 continue
             elif use[0] == "return":
-	        continue
-	    else:
-	        left = is_var(use[2]) and values[use[2]] or int(use[2])
-	        right = is_var(use[3]) and values[use[3]] or int(use[3])
-	        val = arith(use[0], left, right)
-	    if val != values[use[1]]:
-	        values[use[1]] = val
-	        for (use, block) in uses.get(use[1], []):
+                continue
+            else:
+                left = check_var(use[2], values)
+                right = check_var(use[3], values)
+                val = arith(use[0], left, right)
+            if val != values[use[1]]:
+                values[use[1]] = val
+                for (use, block) in uses.get(use[1], []):
                     if block.live:
                         usewl.append(use)
     return values
@@ -205,3 +212,4 @@ def rewrite(uses, values):
                         op[2] = arith(op[0], op[2], op[3])
                         op[0] = "="
                         del op[3]
+
